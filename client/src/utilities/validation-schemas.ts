e40d8369b66c827c640c25b031ab8ded5bed9b38
bowadao <samuelibrahim3029@gmail.com>
2024-07-25 12:39:00 +0100

    input yup validation

--- client/src/utilities/validation-schemas.ts
+++ client/src/utilities/validation-schemas.ts
@@ -1,14 +1,21 @@
 import * as Yup from "yup";
 
 const signupValidationSchema = Yup.object({
-  firstName: Yup.string().required("The first name field is required."),
-  lastName: Yup.string().required("The last name field is required."),
+  firstName: Yup.string()
+    .required("The first name field is required.")
+    .matches(/^\S(.*\S)?$/, "First name is required"),
+  lastName: Yup.string()
+    .required("The last name field is required.")
+    .matches(/^\S(.*\S)?$/, "Last name is required"),
   email: Yup.string()
     .required("The email field is required.")
     .email("Enter a valid email address."),
   password: Yup.string()
     .required("The password field is required.")
-    .min(8, "Password should be at least 8 characters."),
+    .matches(
+      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+      "At least 8 characters, a letter, a number and a special character."
+    ),
 });
 
 const signinValidationSchema = Yup.object({
--- server/utilities/validationSchemas.ts
+++ server/utilities/validationSchemas.ts
@@ -1,14 +1,21 @@
 import * as Yup from "yup";
 
 const RegisterFormSchema = Yup.object({
-  firstName: Yup.string().required("The first name field is required."),
-  lastName: Yup.string().required("The last name field is required."),
+  firstName: Yup.string()
+    .required("The first name field is required.")
+    .matches(/^\S(.*\S)?$/, "First name is required"),
+  lastName: Yup.string()
+    .required("The last name field is required.")
+    .matches(/^\S(.*\S)?$/, "Last name is required"),
   email: Yup.string()
     .required("The email field is required.")
     .email("That is not a valid email format."),
   password: Yup.string()
     .required("The password field is required.")
-    .min(8, "Password should be at least 8 characters."),
+    .matches(
+      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+      "At least 8 characters, a letter, a number and a special character."
+    ),
   accountType: Yup.string().required("Select your business type."),
 });
 
