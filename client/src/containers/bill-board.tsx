9c0b8ba0aab293475522c094c60118a3f55c1667
bowadao <samuelibrahim3029@gmail.com>
2024-08-09 22:22:06 +0100

    tickets

--- client/src/containers/bill-board.tsx
+++ client/src/containers/bill-board.tsx
@@ -1,7 +1,20 @@
+import images from "@/constants/images";
+import Image from "next/image";
+
 const BillBoard = () => {
   return (
-    <section className="paddingX my-12 lg:my-16">
-      <div className="h-[350px] bg-pink-700 w-full" />
+    <section className="block relative paddingX my-12 lg:my-16">
+      <div className="h-[350px] lg:h-[400px] bg-pink-700 w-full">
+        <Image
+          src={images.bowtifyhero1}
+          alt="hero-banner"
+          fill
+          quality={100}
+          priority
+          sizes="100vw"
+          className="object-cover object-center"
+        />
+      </div>
     </section>
   );
 };
